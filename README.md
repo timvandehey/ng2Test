4e719497a6b7a6a7e89fed39359c5a8240fed78e
Tim Vandehey <tim.andehey.gmail.com>
2015-09-14 18:15:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# AngularJS 2 Demo
+
+## Resources used:
+- AngularJS 2.0.0-alpha.20
+- Traceur 0.0.87 with ES6 module loader 0.16.5
+- SystemJS 0.16.7
+- Gulp to start Connect with LiveReload
+
+## Usage
+
+```
+cd ng2
+npm install
+gulp
+
+```
---
+++ src/main.es6.js
@@ -0,0 +1,30 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'ng2'
+})
+@View({
+  template: `
+    <section role="main">
+      <h1>{{ greeting }}, {{ name }}!</h1>
+      <label for="name">Your Name:
+        <input type="text" [value]="name" (keyup)="setName($event.target.value)"/>
+      </label>
+    </section>
+  `
+})
+class App {
+  greeting:string;
+  name:string;
+
+  constructor() {
+    this.greeting = 'Welcome';
+    this.name = 'codejets';
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+}
+
+bootstrap(App);
